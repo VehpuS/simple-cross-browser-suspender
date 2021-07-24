4cd58088b5fe55905f363f5abee0b6d252bf7bb8
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2021-07-24 18:13:44 +0300

    feat: add a manifest version 3 version of the extension, tested on chrome and edge

---
+++ manifest-v3/background.js
@@ -0,0 +1,5 @@
+try {
+  importScripts("./pageSuspender.js");
+} catch (err) {
+  console.error(err);
+}
---
+++ manifest-v3/pageSuspender.js
@@ -0,0 +1,16 @@
+const suspendPrefix = "https://localhost:0/#";
+
+const toggleSuspendUrl = (pageUrl) =>
+  pageUrl.startsWith(suspendPrefix)
+    ? pageUrl.replace(suspendPrefix, "")
+    : `${suspendPrefix}${pageUrl}`;
+
+if (chrome) {
+  chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.update(tab.id, { url: toggleSuspendUrl(tab.url) });
+  });
+} else if (browser) {
+  browser.browserAction.onClicked.addListener((tab) => {
+    browser.tabs.update(tab.id, { url: toggleSuspendUrl(tab.url) });
+  });
+}
